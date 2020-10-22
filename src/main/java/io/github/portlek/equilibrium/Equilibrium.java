91303114e376710f9708ddaf7154f71679bf9768
Hasan Demirtaş <utsukushihito@outlook.com>
2020-10-22 17:05:34 +0300

    New development. (#24)

--- src/main/java/io/github/portlek/equilibrium/Equilibrium.java
+++ src/main/java/io/github/portlek/equilibrium/Equilibrium.java
@@ -29,160 +29,229 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.function.BiFunction;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import java.util.function.BiPredicate;
 import org.jetbrains.annotations.NotNull;
 
-@RequiredArgsConstructor
+/**
+ * a class that allows you to use Java operators.
+ */
 public enum Equilibrium {
 
-    EQUALS(Objects::equals, "=", "=="),
-    NOT_EQUALS((leftObject, rightObject) ->
-        !leftObject.equals(rightObject), "!="),
-    BIGGER((leftObject, rightObject) -> {
-        if (!(leftObject instanceof Number) || !(rightObject instanceof Number)) {
-            return false;
-        }
-        final Number left = (Number) leftObject;
-        final Number right = (Number) rightObject;
-        if (left instanceof Double) {
-            return left.doubleValue() > right.doubleValue();
-        }
-        if (left instanceof Integer) {
-            return left.intValue() > right.intValue();
-        }
-        if (left instanceof Long) {
-            return left.longValue() > right.longValue();
-        }
-        if (left instanceof Float) {
-            return left.floatValue() > right.floatValue();
-        }
-        if (left instanceof Short) {
-            return (int) left.shortValue() > (int) right.shortValue();
-        }
-        if (left instanceof Byte) {
-            return (int) left.byteValue() > (int) right.byteValue();
-        }
-        return false;
-    }, ">"),
-    BIGGER_AND_EQUALS((leftObject, rightObject) -> {
-        if (!(leftObject instanceof Number) || !(rightObject instanceof Number)) {
-            return false;
-        }
-        final Number left = (Number) leftObject;
-        final Number right = (Number) rightObject;
-        if (left instanceof Double) {
-            return left.doubleValue() >= right.doubleValue();
-        }
-        if (left instanceof Integer) {
-            return left.intValue() >= right.intValue();
-        }
-        if (left instanceof Long) {
-            return left.longValue() >= right.longValue();
-        }
-        if (left instanceof Float) {
-            return left.floatValue() >= right.floatValue();
-        }
-        if (left instanceof Short) {
-            return (int) left.shortValue() >= (int) right.shortValue();
-        }
-        if (left instanceof Byte) {
-            return (int) left.byteValue() >= (int) right.byteValue();
-        }
-        return false;
-    }, "=>", ">="),
-    LESS((leftObject, rightObject) -> {
-        if (!(leftObject instanceof Number) || !(rightObject instanceof Number)) {
-            return false;
-        }
-        final Number left = (Number) leftObject;
-        final Number right = (Number) rightObject;
-        if (left instanceof Double) {
-            return left.doubleValue() < right.doubleValue();
-        }
-        if (left instanceof Integer) {
-            return left.intValue() < right.intValue();
-        }
-        if (left instanceof Long) {
-            return left.longValue() < right.longValue();
-        }
-        if (left instanceof Float) {
-            return left.floatValue() < right.floatValue();
-        }
-        if (left instanceof Short) {
-            return (int) left.shortValue() < (int) right.shortValue();
-        }
-        if (left instanceof Byte) {
-            return (int) left.byteValue() < (int) right.byteValue();
-        }
-        return false;
-    }, "<"),
-    LESS_OR_EQUALS((leftObject, rightObject) -> {
-        if (!(leftObject instanceof Number) || !(rightObject instanceof Number)) {
-            return false;
-        }
-        final Number left = (Number) leftObject;
-        final Number right = (Number) rightObject;
-        if (left instanceof Double) {
-            return left.doubleValue() <= right.doubleValue();
-        }
-        if (left instanceof Integer) {
-            return left.intValue() <= right.intValue();
-        }
-        if (left instanceof Long) {
-            return left.longValue() <= right.longValue();
-        }
-        if (left instanceof Float) {
-            return left.floatValue() <= right.floatValue();
-        }
-        if (left instanceof Short) {
-            return (int) left.shortValue() <= (int) right.shortValue();
-        }
-        if (left instanceof Byte) {
-            return (int) left.byteValue() <= (int) right.byteValue();
-        }
-        return false;
-    }, "<=", "=<"),
-    INSTANCE_OF((leftObject, rightObject) -> {
-        if (!(leftObject instanceof Class<?>) || !(rightObject instanceof Class<?>)) {
-            return false;
-        }
-        return ((Class<?>) rightObject).isAssignableFrom((Class<?>) leftObject);
-    }, "is", "instance of"),
-    NOT_INSTANCE_OF((leftObject, rightObject) -> {
-        if (!(leftObject instanceof Class<?>) || !(rightObject instanceof Class<?>)) {
-            return false;
-        }
-        return !((Class<?>) rightObject).isAssignableFrom((Class<?>) leftObject);
-    }, "isnt", "isn't", "isnot", "is not", "not instance of"),
-    NOTHING((leftObject, rightObject) -> false);
-
-    @NotNull
-    private final List<String> operators;
-
-    @NotNull
-    private final BiFunction<Object, Object, Boolean> func;
-
-    Equilibrium(final BiFunction<Object, Object, Boolean> func, @NotNull final String... operators) {
-        this(Arrays.asList(operators), func);
+  /**
+   * checks if the object equals.
+   */
+  EQUALS(Objects::equals, "=", "=="),
+  /**
+   * checks if the object not equals.
+   */
+  NOT_EQUALS((leftObject, rightObject) ->
+    !leftObject.equals(rightObject), "!="),
+  /**
+   * checks if the number is bigger than the other.
+   */
+  BIGGER((leftObject, rightObject) -> {
+    if (!(leftObject instanceof Number) || !(rightObject instanceof Number)) {
+      return false;
     }
-
-    @NotNull
-    public static Equilibrium fromString(@NotNull final String operator) {
-        return Arrays.stream(Equilibrium.values())
-            .filter(equilibrium -> equilibrium.getOperators().contains(operator))
-            .findFirst()
-            .orElse(Equilibrium.NOTHING);
+    final Number left = (Number) leftObject;
+    final Number right = (Number) rightObject;
+    if (left instanceof Double) {
+      return left.doubleValue() > right.doubleValue();
     }
-
-    public boolean control(@NotNull final Object leftObject, @NotNull final Object rightObject) {
-        return this.func.apply(leftObject, rightObject);
+    if (left instanceof Integer) {
+      return left.intValue() > right.intValue();
     }
-
-    @NotNull
-    public List<String> getOperators() {
-        return Collections.unmodifiableList(this.operators);
+    if (left instanceof Long) {
+      return left.longValue() > right.longValue();
     }
+    if (left instanceof Float) {
+      return left.floatValue() > right.floatValue();
+    }
+    if (left instanceof Short) {
+      return (int) left.shortValue() > (int) right.shortValue();
+    }
+    if (left instanceof Byte) {
+      return (int) left.byteValue() > (int) right.byteValue();
+    }
+    return false;
+  }, ">"),
+  /**
+   * checks if the number is equal or bigger than the other.
+   */
+  BIGGER_AND_EQUALS((leftObject, rightObject) -> {
+    if (!(leftObject instanceof Number) || !(rightObject instanceof Number)) {
+      return false;
+    }
+    final Number left = (Number) leftObject;
+    final Number right = (Number) rightObject;
+    if (left instanceof Double) {
+      return left.doubleValue() >= right.doubleValue();
+    }
+    if (left instanceof Integer) {
+      return left.intValue() >= right.intValue();
+    }
+    if (left instanceof Long) {
+      return left.longValue() >= right.longValue();
+    }
+    if (left instanceof Float) {
+      return left.floatValue() >= right.floatValue();
+    }
+    if (left instanceof Short) {
+      return (int) left.shortValue() >= (int) right.shortValue();
+    }
+    if (left instanceof Byte) {
+      return (int) left.byteValue() >= (int) right.byteValue();
+    }
+    return false;
+  }, "=>", ">="),
+  /**
+   * checks if the number is lower than the other.
+   */
+  LESS((leftObject, rightObject) -> {
+    if (!(leftObject instanceof Number) || !(rightObject instanceof Number)) {
+      return false;
+    }
+    final Number left = (Number) leftObject;
+    final Number right = (Number) rightObject;
+    if (left instanceof Double) {
+      return left.doubleValue() < right.doubleValue();
+    }
+    if (left instanceof Integer) {
+      return left.intValue() < right.intValue();
+    }
+    if (left instanceof Long) {
+      return left.longValue() < right.longValue();
+    }
+    if (left instanceof Float) {
+      return left.floatValue() < right.floatValue();
+    }
+    if (left instanceof Short) {
+      return (int) left.shortValue() < (int) right.shortValue();
+    }
+    if (left instanceof Byte) {
+      return (int) left.byteValue() < (int) right.byteValue();
+    }
+    return false;
+  }, "<"),
+  /**
+   * checks if the number is equal or lower than the other.
+   */
+  LESS_OR_EQUALS((leftObject, rightObject) -> {
+    if (!(leftObject instanceof Number) || !(rightObject instanceof Number)) {
+      return false;
+    }
+    final Number left = (Number) leftObject;
+    final Number right = (Number) rightObject;
+    if (left instanceof Double) {
+      return left.doubleValue() <= right.doubleValue();
+    }
+    if (left instanceof Integer) {
+      return left.intValue() <= right.intValue();
+    }
+    if (left instanceof Long) {
+      return left.longValue() <= right.longValue();
+    }
+    if (left instanceof Float) {
+      return left.floatValue() <= right.floatValue();
+    }
+    if (left instanceof Short) {
+      return (int) left.shortValue() <= (int) right.shortValue();
+    }
+    if (left instanceof Byte) {
+      return (int) left.byteValue() <= (int) right.byteValue();
+    }
+    return false;
+  }, "<=", "=<"),
+  /**
+   * checks if the object instance of the other.
+   */
+  INSTANCE_OF((leftObject, rightObject) -> {
+    if (!(leftObject instanceof Class<?>) || !(rightObject instanceof Class<?>)) {
+      return false;
+    }
+    return ((Class<?>) rightObject).isAssignableFrom((Class<?>) leftObject);
+  }, "is", "instance of"),
+  /**
+   * checks if the object not instance of the other.
+   */
+  NOT_INSTANCE_OF((leftObject, rightObject) -> {
+    if (!(leftObject instanceof Class<?>) || !(rightObject instanceof Class<?>)) {
+      return false;
+    }
+    return !((Class<?>) rightObject).isAssignableFrom((Class<?>) leftObject);
+  }, "isnt", "isn't", "isnot", "is not", "not instance of"),
+  /**
+   * nothing.
+   */
+  NOTHING((leftObject, rightObject) -> false);
 
+  /**
+   * the operators.
+   */
+  @NotNull
+  private final List<String> operators;
+
+  /**
+   * the function.
+   */
+  @NotNull
+  private final BiPredicate<Object, Object> predicate;
+
+  /**
+   * ctor.
+   *
+   * @param operators the operators.
+   * @param predicate the function.
+   */
+  Equilibrium(@NotNull final List<String> operators, @NotNull final BiPredicate<Object, Object> predicate) {
+    this.operators = operators;
+    this.predicate = predicate;
+  }
+
+  /**
+   * ctor.
+   *
+   * @param predicate the function.
+   * @param operators the operators.
+   */
+  Equilibrium(@NotNull final BiPredicate<Object, Object> predicate, @NotNull final String... operators) {
+    this(Arrays.asList(operators), predicate);
+  }
+
+  /**
+   * obtains an {@code this} from the string.
+   *
+   * @param operator the operator to get.
+   *
+   * @return an {@code this} object instance.
+   */
+  @NotNull
+  public static Equilibrium fromString(@NotNull final String operator) {
+    return Arrays.stream(Equilibrium.values())
+      .filter(equilibrium -> equilibrium.getOperators().contains(operator))
+      .findFirst()
+      .orElse(Equilibrium.NOTHING);
+  }
+
+  /**
+   * controls the predicate.
+   *
+   * @param leftObject the left object to control.
+   * @param rightObject the right object to control.
+   *
+   * @return {@code true} if the predicate returns {@code true}.
+   */
+  public boolean control(@NotNull final Object leftObject, @NotNull final Object rightObject) {
+    return this.predicate.test(leftObject, rightObject);
+  }
+
+  /**
+   * obtains the operators.
+   *
+   * @return the operator list.
+   */
+  @NotNull
+  public List<String> getOperators() {
+    return Collections.unmodifiableList(this.operators);
+  }
 }
--- src/test/java/io/github/portlek/equilibrium/EquilibriumTest.java
+++ src/test/java/io/github/portlek/equilibrium/EquilibriumTest.java
@@ -25,166 +25,163 @@
 
 package io.github.portlek.equilibrium;
 
-import org.hamcrest.MatcherAssert;
 import org.hamcrest.core.IsEqual;
 import org.hamcrest.core.IsNot;
 import org.junit.jupiter.api.Test;
+import org.llorllale.cactoos.matchers.Assertion;
 import org.llorllale.cactoos.matchers.IsTrue;
 
 final class EquilibriumTest {
 
-    @Test
-    void fromString() {
-        final Equilibrium equals = Equilibrium.fromString("==");
-        final Equilibrium notEquals = Equilibrium.fromString("!=");
-        final Equilibrium bigger = Equilibrium.fromString(">");
-        final Equilibrium biggerOrEquals = Equilibrium.fromString(">=");
-        final Equilibrium less = Equilibrium.fromString("<");
-        final Equilibrium lessOfEquals = Equilibrium.fromString("<=");
-        final Equilibrium instanceOf = Equilibrium.fromString("is");
-        final Equilibrium notInstanceOf = Equilibrium.fromString("is not");
-        final Equilibrium nothing = Equilibrium.fromString("asd");
+  @Test
+  void fromString() {
+    final Equilibrium equals = Equilibrium.fromString("==");
+    final Equilibrium notEquals = Equilibrium.fromString("!=");
+    final Equilibrium bigger = Equilibrium.fromString(">");
+    final Equilibrium biggerOrEquals = Equilibrium.fromString(">=");
+    final Equilibrium less = Equilibrium.fromString("<");
+    final Equilibrium lessOfEquals = Equilibrium.fromString("<=");
+    final Equilibrium instanceOf = Equilibrium.fromString("is");
+    final Equilibrium notInstanceOf = Equilibrium.fromString("is not");
+    final Equilibrium nothing = Equilibrium.fromString("asd");
+    new Assertion<>(
+      "Couldn't parse as equals!",
+      equals,
+      new IsEqual<>(Equilibrium.EQUALS)
+    ).affirm();
+    new Assertion<>(
+      "Couldn't parse as notEquals!",
+      notEquals,
+      new IsEqual<>(Equilibrium.NOT_EQUALS)
+    ).affirm();
+    new Assertion<>(
+      "Couldn't parse as bigger!",
+      bigger,
+      new IsEqual<>(Equilibrium.BIGGER)
+    ).affirm();
+    new Assertion<>(
+      "Couldn't parse as biggerOrEquals!",
+      biggerOrEquals,
+      new IsEqual<>(Equilibrium.BIGGER_AND_EQUALS)
+    ).affirm();
+    new Assertion<>(
+      "Couldn't parse as less!",
+      less,
+      new IsEqual<>(Equilibrium.LESS)
+    ).affirm();
+    new Assertion<>(
+      "Couldn't parse as lessOfEquals!",
+      lessOfEquals,
+      new IsEqual<>(Equilibrium.LESS_OR_EQUALS)
+    ).affirm();
+    new Assertion<>(
+      "Couldn't parse as instanceOf!",
+      instanceOf,
+      new IsEqual<>(Equilibrium.INSTANCE_OF)
+    ).affirm();
+    new Assertion<>(
+      "Couldn't parse as notInstanceOf!",
+      notInstanceOf,
+      new IsEqual<>(Equilibrium.NOT_INSTANCE_OF)
+    ).affirm();
+    new Assertion<>(
+      "Couldn't parse as nothing!",
+      nothing,
+      new IsEqual<>(Equilibrium.NOTHING)
+    ).affirm();
+  }
 
-        MatcherAssert.assertThat(
-            "Couldn't parse as equals!",
-            equals,
-            new IsEqual<>(Equilibrium.EQUALS)
-        );
-        MatcherAssert.assertThat(
-            "Couldn't parse as notEquals!",
-            notEquals,
-            new IsEqual<>(Equilibrium.NOT_EQUALS)
-        );
-        MatcherAssert.assertThat(
-            "Couldn't parse as bigger!",
-            bigger,
-            new IsEqual<>(Equilibrium.BIGGER)
-        );
-        MatcherAssert.assertThat(
-            "Couldn't parse as biggerOrEquals!",
-            biggerOrEquals,
-            new IsEqual<>(Equilibrium.BIGGER_AND_EQUALS)
-        );
-        MatcherAssert.assertThat(
-            "Couldn't parse as less!",
-            less,
-            new IsEqual<>(Equilibrium.LESS)
-        );
-        MatcherAssert.assertThat(
-            "Couldn't parse as lessOfEquals!",
-            lessOfEquals,
-            new IsEqual<>(Equilibrium.LESS_OR_EQUALS)
-        );
-        MatcherAssert.assertThat(
-            "Couldn't parse as instanceOf!",
-            instanceOf,
-            new IsEqual<>(Equilibrium.INSTANCE_OF)
-        );
-        MatcherAssert.assertThat(
-            "Couldn't parse as notInstanceOf!",
-            notInstanceOf,
-            new IsEqual<>(Equilibrium.NOT_INSTANCE_OF)
-        );
-        MatcherAssert.assertThat(
-            "Couldn't parse as nothing!",
-            nothing,
-            new IsEqual<>(Equilibrium.NOTHING)
-        );
-    }
+  @Test
+  void control() {
+    final Equilibrium equals = Equilibrium.fromString("==");
+    final Equilibrium notEquals = Equilibrium.fromString("!=");
+    final Equilibrium bigger = Equilibrium.fromString(">");
+    final Equilibrium biggerOrEquals = Equilibrium.fromString(">=");
+    final Equilibrium less = Equilibrium.fromString("<");
+    final Equilibrium lessOfEquals = Equilibrium.fromString("<=");
+    final Equilibrium instanceOf = Equilibrium.fromString("is");
+    final Equilibrium notInstanceOf = Equilibrium.fromString("is not");
+    final Equilibrium nothing = Equilibrium.fromString("asd");
+    new Assertion<>(
+      "Objects are not equal!",
+      equals.control("test", "test"),
+      new IsTrue()
+    ).affirm();
+    new Assertion<>(
+      "Objects are equal!",
+      notEquals.control("test", "test-2"),
+      new IsTrue()
+    ).affirm();
+    new Assertion<>(
+      "Left is not bigger than right!",
+      bigger.control(1, 0),
+      new IsTrue()
+    ).affirm();
+    new Assertion<>(
+      "Left is not bigger than right!",
+      bigger.control(1.1d, 1.0d),
+      new IsTrue()
+    ).affirm();
+    new Assertion<>(
+      "Left is not bigger or equals to the right number!",
+      biggerOrEquals.control(1, 0),
+      new IsTrue()
+    ).affirm();
+    new Assertion<>(
+      "Left is not bigger or equals to the right number!",
+      biggerOrEquals.control(1.1d, 1.0d),
+      new IsTrue()
+    ).affirm();
+    new Assertion<>(
+      "Left is not less than right!",
+      less.control(0, 1),
+      new IsTrue()
+    ).affirm();
+    new Assertion<>(
+      "Left is not less than right!",
+      less.control(1.0d, 1.1d),
+      new IsTrue()
+    ).affirm();
+    new Assertion<>(
+      "Left is not less or equals to the right number!",
+      lessOfEquals.control(0, 1),
+      new IsTrue()
+    ).affirm();
+    new Assertion<>(
+      "Left is not less or equals to the right number!",
+      lessOfEquals.control(1.0d, 1.1d),
+      new IsTrue()
+    ).affirm();
+    new Assertion<>(
+      "Right is not assignable from left object!",
+      instanceOf.control(EquilibriumTest.TestImpl.class, EquilibriumTest.ITest.class),
+      new IsTrue()
+    ).affirm();
+    new Assertion<>(
+      "Right is assignable from left object!",
+      notInstanceOf.control(EquilibriumTest.AnotherTestImpl.class, EquilibriumTest.ITest.class),
+      new IsTrue()
+    ).affirm();
+    new Assertion<>(
+      "Just nothing!",
+      nothing.control("test", "test"),
+      new IsNot<>(new IsTrue())
+    ).affirm();
+  }
 
-    @Test
-    void control() {
-        final Equilibrium equals = Equilibrium.fromString("==");
-        final Equilibrium notEquals = Equilibrium.fromString("!=");
-        final Equilibrium bigger = Equilibrium.fromString(">");
-        final Equilibrium biggerOrEquals = Equilibrium.fromString(">=");
-        final Equilibrium less = Equilibrium.fromString("<");
-        final Equilibrium lessOfEquals = Equilibrium.fromString("<=");
-        final Equilibrium instanceOf = Equilibrium.fromString("is");
-        final Equilibrium notInstanceOf = Equilibrium.fromString("is not");
-        final Equilibrium nothing = Equilibrium.fromString("asd");
+  private interface ITest {
 
-        MatcherAssert.assertThat(
-            "Objects are not equal!",
-            equals.control("test", "test"),
-            new IsTrue()
-        );
-        MatcherAssert.assertThat(
-            "Objects are equal!",
-            notEquals.control("test", "test-2"),
-            new IsTrue()
-        );
-        MatcherAssert.assertThat(
-            "Left is not bigger than right!",
-            bigger.control(1, 0),
-            new IsTrue()
-        );
-        MatcherAssert.assertThat(
-            "Left is not bigger than right!",
-            bigger.control(1.1d, 1.0d),
-            new IsTrue()
-        );
-        MatcherAssert.assertThat(
-            "Left is not bigger or equals to the right number!",
-            biggerOrEquals.control(1, 0),
-            new IsTrue()
-        );
-        MatcherAssert.assertThat(
-            "Left is not bigger or equals to the right number!",
-            biggerOrEquals.control(1.1d, 1.0d),
-            new IsTrue()
-        );
-        MatcherAssert.assertThat(
-            "Left is not less than right!",
-            less.control(0, 1),
-            new IsTrue()
-        );
-        MatcherAssert.assertThat(
-            "Left is not less than right!",
-            less.control(1.0d, 1.1d),
-            new IsTrue()
-        );
-        MatcherAssert.assertThat(
-            "Left is not less or equals to the right number!",
-            lessOfEquals.control(0, 1),
-            new IsTrue()
-        );
-        MatcherAssert.assertThat(
-            "Left is not less or equals to the right number!",
-            lessOfEquals.control(1.0d, 1.1d),
-            new IsTrue()
-        );
-        MatcherAssert.assertThat(
-            "Right is not assignable from left object!",
-            instanceOf.control(EquilibriumTest.TestImpl.class, EquilibriumTest.ITest.class),
-            new IsTrue()
-        );
-        MatcherAssert.assertThat(
-            "Right is assignable from left object!",
-            notInstanceOf.control(EquilibriumTest.AnotherTestImpl.class, EquilibriumTest.ITest.class),
-            new IsTrue()
-        );
-        MatcherAssert.assertThat(
-            "Just nothing!",
-            nothing.control("test", "test"),
-            new IsNot<>(new IsTrue())
-        );
-    }
+  }
 
-    private interface ITest {
+  private interface AnotherITest {
 
-    }
+  }
 
-    private interface AnotherITest {
+  private static final class TestImpl implements EquilibriumTest.ITest {
 
-    }
+  }
 
-    private static final class TestImpl implements EquilibriumTest.ITest {
+  private static final class AnotherTestImpl implements EquilibriumTest.AnotherITest {
 
-    }
-
-    private static final class AnotherTestImpl implements EquilibriumTest.AnotherITest {
-
-    }
-
+  }
 }
