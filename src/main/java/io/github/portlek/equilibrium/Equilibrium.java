749b2ca40943462f96673dc024eeddc69ce903f7
Hasan Demirtaş <utsukushihito@outlook.com>
2021-01-24 17:22:38 +0300

    Dev (#41)
    * simplify.
    
    * simplify.
    
    * Update Equilibrium.java
    
    * Update package-info.java

--- src/main/java/io/github/portlek/equilibrium/Equilibrium.java
+++ src/main/java/io/github/portlek/equilibrium/Equilibrium.java
@@ -28,7 +28,6 @@ package io.github.portlek.equilibrium;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.function.BiPredicate;
 import org.jetbrains.annotations.NotNull;
 
@@ -40,150 +39,40 @@ public enum Equilibrium {
   /**
    * checks if the object equals.
    */
-  EQUALS(Objects::equals, "=", "=="),
+  EQUALS(Utilities::equals, "Utilities.equals(%s, %s)", "=", "=="),
   /**
    * checks if the object not equals.
    */
-  NOT_EQUALS((leftObject, rightObject) ->
-    !leftObject.equals(rightObject), "!="),
+  NOT_EQUALS(Utilities::notEqual, "Utilities.notEqual(%s, %s)", "!="),
   /**
    * checks if the number is bigger than the other.
    */
-  BIGGER((leftObject, rightObject) -> {
-    if (!(leftObject instanceof Number) || !(rightObject instanceof Number)) {
-      return false;
-    }
-    final Number left = (Number) leftObject;
-    final Number right = (Number) rightObject;
-    if (left instanceof Double) {
-      return left.doubleValue() > right.doubleValue();
-    }
-    if (left instanceof Integer) {
-      return left.intValue() > right.intValue();
-    }
-    if (left instanceof Long) {
-      return left.longValue() > right.longValue();
-    }
-    if (left instanceof Float) {
-      return left.floatValue() > right.floatValue();
-    }
-    if (left instanceof Short) {
-      return (int) left.shortValue() > (int) right.shortValue();
-    }
-    if (left instanceof Byte) {
-      return (int) left.byteValue() > (int) right.byteValue();
-    }
-    return false;
-  }, ">"),
+  BIGGER(Utilities::isBigger, "Utilities.isBigger(%s, %s)", ">"),
   /**
    * checks if the number is equal or bigger than the other.
    */
-  BIGGER_AND_EQUALS((leftObject, rightObject) -> {
-    if (!(leftObject instanceof Number) || !(rightObject instanceof Number)) {
-      return false;
-    }
-    final Number left = (Number) leftObject;
-    final Number right = (Number) rightObject;
-    if (left instanceof Double) {
-      return left.doubleValue() >= right.doubleValue();
-    }
-    if (left instanceof Integer) {
-      return left.intValue() >= right.intValue();
-    }
-    if (left instanceof Long) {
-      return left.longValue() >= right.longValue();
-    }
-    if (left instanceof Float) {
-      return left.floatValue() >= right.floatValue();
-    }
-    if (left instanceof Short) {
-      return (int) left.shortValue() >= (int) right.shortValue();
-    }
-    if (left instanceof Byte) {
-      return (int) left.byteValue() >= (int) right.byteValue();
-    }
-    return false;
-  }, "=>", ">="),
+  BIGGER_AND_EQUALS(Utilities::isBiggerEquals, "Utilities.isBiggerEquals(%s, %s)", "=>", ">="),
   /**
    * checks if the number is lower than the other.
    */
-  LESS((leftObject, rightObject) -> {
-    if (!(leftObject instanceof Number) || !(rightObject instanceof Number)) {
-      return false;
-    }
-    final Number left = (Number) leftObject;
-    final Number right = (Number) rightObject;
-    if (left instanceof Double) {
-      return left.doubleValue() < right.doubleValue();
-    }
-    if (left instanceof Integer) {
-      return left.intValue() < right.intValue();
-    }
-    if (left instanceof Long) {
-      return left.longValue() < right.longValue();
-    }
-    if (left instanceof Float) {
-      return left.floatValue() < right.floatValue();
-    }
-    if (left instanceof Short) {
-      return (int) left.shortValue() < (int) right.shortValue();
-    }
-    if (left instanceof Byte) {
-      return (int) left.byteValue() < (int) right.byteValue();
-    }
-    return false;
-  }, "<"),
+  LESS(Utilities::isLess, "Utilities.isLess(%s, %s)", "<"),
   /**
    * checks if the number is equal or lower than the other.
    */
-  LESS_OR_EQUALS((leftObject, rightObject) -> {
-    if (!(leftObject instanceof Number) || !(rightObject instanceof Number)) {
-      return false;
-    }
-    final Number left = (Number) leftObject;
-    final Number right = (Number) rightObject;
-    if (left instanceof Double) {
-      return left.doubleValue() <= right.doubleValue();
-    }
-    if (left instanceof Integer) {
-      return left.intValue() <= right.intValue();
-    }
-    if (left instanceof Long) {
-      return left.longValue() <= right.longValue();
-    }
-    if (left instanceof Float) {
-      return left.floatValue() <= right.floatValue();
-    }
-    if (left instanceof Short) {
-      return (int) left.shortValue() <= (int) right.shortValue();
-    }
-    if (left instanceof Byte) {
-      return (int) left.byteValue() <= (int) right.byteValue();
-    }
-    return false;
-  }, "<=", "=<"),
+  LESS_OR_EQUALS(Utilities::isLessEquals, "Utilities.isLessEquals(%s, %s)", "<=", "=<"),
   /**
    * checks if the object instance of the other.
    */
-  INSTANCE_OF((leftObject, rightObject) -> {
-    if (!(leftObject instanceof Class<?>) || !(rightObject instanceof Class<?>)) {
-      return false;
-    }
-    return ((Class<?>) rightObject).isAssignableFrom((Class<?>) leftObject);
-  }, "is", "instance of"),
+  INSTANCE_OF(Utilities::instanceOf, "Utilities.instanceOf(%s, %s)", "is", "instance of"),
   /**
    * checks if the object not instance of the other.
    */
-  NOT_INSTANCE_OF((leftObject, rightObject) -> {
-    if (!(leftObject instanceof Class<?>) || !(rightObject instanceof Class<?>)) {
-      return false;
-    }
-    return !((Class<?>) rightObject).isAssignableFrom((Class<?>) leftObject);
-  }, "isnt", "isn't", "isnot", "is not", "not instance of"),
+  NOT_INSTANCE_OF(Utilities::noInstanceOf, "Utilities.noInstanceOf(%s, %s)",
+    "isnt", "isn't", "isnot", "is not", "not instance of"),
   /**
    * nothing.
    */
-  NOTHING((leftObject, rightObject) -> false);
+  NOTHING((leftObject, rightObject) -> false, "");
 
   /**
    * the operators.
@@ -198,14 +87,22 @@ public enum Equilibrium {
   private final BiPredicate<Object, Object> predicate;
 
   /**
+   * the to string.
+   */
+  @NotNull
+  private final String toString;
+
+  /**
    * ctor.
    *
    * @param operators the operators.
    * @param predicate the function.
    */
-  Equilibrium(@NotNull final List<String> operators, @NotNull final BiPredicate<Object, Object> predicate) {
+  Equilibrium(@NotNull final List<String> operators, @NotNull final BiPredicate<Object, Object> predicate,
+              @NotNull final String toString) {
     this.operators = operators;
     this.predicate = predicate;
+    this.toString = toString;
   }
 
   /**
@@ -214,8 +111,9 @@ public enum Equilibrium {
    * @param predicate the function.
    * @param operators the operators.
    */
-  Equilibrium(@NotNull final BiPredicate<Object, Object> predicate, @NotNull final String... operators) {
-    this(Arrays.asList(operators), predicate);
+  Equilibrium(@NotNull final BiPredicate<Object, Object> predicate, @NotNull final String toString,
+              @NotNull final String... operators) {
+    this(Arrays.asList(operators), predicate, toString);
   }
 
   /**
@@ -253,5 +151,10 @@ public enum Equilibrium {
   @NotNull
   public List<String> getOperators() {
     return Collections.unmodifiableList(this.operators);
+  }
+
+  @Override
+  public String toString() {
+    return this.toString;
   }
 }
---
+++ src/main/java/io/github/portlek/equilibrium/Utilities.java
@@ -0,0 +1,186 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Hasan Demirtaş
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+package io.github.portlek.equilibrium;
+
+import java.util.Objects;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * a class that contains utility methods.
+ */
+public final class Utilities {
+
+  /**
+   * ctor.
+   */
+  private Utilities() {
+  }
+
+  /**
+   * checks if the given {@code leftObject} is instance of {@code rightObject}.
+   *
+   * @param leftObject the left object to check.
+   * @param rightObject the right object to check.
+   *
+   * @return {@code true} if the left object is instance of {@code rightObject}.
+   */
+  public static boolean instanceOf(@NotNull final Object leftObject, @NotNull final Object rightObject) {
+    if (!(leftObject instanceof Class<?>) || !(rightObject instanceof Class<?>)) {
+      return false;
+    }
+    return ((Class<?>) rightObject).isAssignableFrom((Class<?>) leftObject);
+  }
+
+  /**
+   * checks if the given {@code leftObject} is not instance of {@code rightObject}.
+   *
+   * @param leftObject the left object to check.
+   * @param rightObject the right object to check.
+   *
+   * @return {@code true} if the left object is not instance of {@code rightObject}.
+   */
+  public static boolean noInstanceOf(@NotNull final Object leftObject, @NotNull final Object rightObject) {
+    return !Utilities.instanceOf(leftObject, rightObject);
+  }
+
+  /**
+   * checks if the given {@code leftObject} is bigger than {@code rightObject}.
+   *
+   * @param leftObject the left object to check.
+   * @param rightObject the right object to check.
+   *
+   * @return {@code true} if the left object is bigger than the right object.
+   */
+  public static boolean isBigger(@NotNull final Object leftObject, @NotNull final Object rightObject) {
+    if (!(leftObject instanceof Number) || !(rightObject instanceof Number)) {
+      return false;
+    }
+    final Number left = (Number) leftObject;
+    final Number right = (Number) rightObject;
+    if (left instanceof Double) {
+      return left.doubleValue() > right.doubleValue();
+    }
+    if (left instanceof Integer) {
+      return left.intValue() > right.intValue();
+    }
+    if (left instanceof Long) {
+      return left.longValue() > right.longValue();
+    }
+    if (left instanceof Float) {
+      return left.floatValue() > right.floatValue();
+    }
+    if (left instanceof Short) {
+      return (int) left.shortValue() > (int) right.shortValue();
+    }
+    if (left instanceof Byte) {
+      return (int) left.byteValue() > (int) right.byteValue();
+    }
+    return false;
+  }
+
+  /**
+   * checks if the given {@code leftObject} is bigger equals than {@code rightObject}.
+   *
+   * @param leftObject the left object to check.
+   * @param rightObject the right object to check.
+   *
+   * @return {@code true} if the left object is bigger equals than the right object.
+   */
+  public static boolean isBiggerEquals(@NotNull final Object leftObject, @NotNull final Object rightObject) {
+    if (!(leftObject instanceof Number) || !(rightObject instanceof Number)) {
+      return false;
+    }
+    final Number left = (Number) leftObject;
+    final Number right = (Number) rightObject;
+    if (left instanceof Double) {
+      return left.doubleValue() >= right.doubleValue();
+    }
+    if (left instanceof Integer) {
+      return left.intValue() >= right.intValue();
+    }
+    if (left instanceof Long) {
+      return left.longValue() >= right.longValue();
+    }
+    if (left instanceof Float) {
+      return left.floatValue() >= right.floatValue();
+    }
+    if (left instanceof Short) {
+      return (int) left.shortValue() >= (int) right.shortValue();
+    }
+    if (left instanceof Byte) {
+      return (int) left.byteValue() >= (int) right.byteValue();
+    }
+    return false;
+  }
+
+  /**
+   * checks if the given {@code leftObject} is less than {@code rightObject}.
+   *
+   * @param leftObject the left object to check.
+   * @param rightObject the right object to check.
+   *
+   * @return {@code true} if the left object is less than the right object.
+   */
+  public static boolean isLess(@NotNull final Object leftObject, @NotNull final Object rightObject) {
+    return !Utilities.isBiggerEquals(leftObject, rightObject);
+  }
+
+  /**
+   * checks if the given {@code leftObject} is less equals than {@code rightObject}.
+   *
+   * @param leftObject the left object to check.
+   * @param rightObject the right object to check.
+   *
+   * @return {@code true} if the left object is less equals than the right object.
+   */
+  public static boolean isLessEquals(@NotNull final Object leftObject, @NotNull final Object rightObject) {
+    return !Utilities.isBigger(leftObject, rightObject);
+  }
+
+  /**
+   * checks if the given {@code leftObject} equals {@code rightObject}.
+   *
+   * @param leftObject the left object to check.
+   * @param rightObject the right object to check.
+   *
+   * @return {@code true} if the left object equals {@code rightObject}.
+   */
+  public static boolean equals(@NotNull final Object leftObject, @NotNull final Object rightObject) {
+    return Objects.equals(leftObject, rightObject);
+  }
+
+  /**
+   * checks if the given {@code leftObject} not equals {@code rightObject}.
+   *
+   * @param leftObject the left object to check.
+   * @param rightObject the right object to check.
+   *
+   * @return {@code true} if the left object not equals right object.
+   */
+  public static boolean notEqual(@NotNull final Object leftObject, @NotNull final Object rightObject) {
+    return !Utilities.equals(leftObject, rightObject);
+  }
+}
